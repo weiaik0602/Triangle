2e8ac51f746e9c227402d77fdb514b0a46adb6b8
Ng Wei Aik <ng_weiaik@yahoo.com>
2017-06-05 12:55:57 +0800

    Created Triangle project

---
+++ src/triangle.h
@@ -0,0 +1,5 @@
+#ifndef _TRIANGLE_H
+#define _TRIANGLE_H
+
+
+#endif // _TRIANGLE_H
---
+++ test/test_triangle.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "triangle.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_triangle_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement triangle");
+}
